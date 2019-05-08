7b7ae6b705bf52397c86b163080282aecf9002c8
Naresh Bhatia <naresh.archfirst@gmail.com>
2019-05-08 08:07:59 -0400

    Mocked out console.error to prevent noisy output from ErrorBoundary

--- src/components/ErrorBoundary/ErrorBoundary.test.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.test.tsx
@@ -5,6 +5,17 @@ import { ErrorBoundary } from './ErrorBoundary';
 const goodBoyText = 'I am a good boy';
 const badBoyText = 'I am a bad boy';
 
+beforeEach(() => {
+    // When an error is thrown a bunch of console.errors are called even though
+    // the error boundary handles the error. This makes the test output noisy,
+    // so we'll mock out console.error
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+});
+
+afterEach(() => {
+    console.error.mockRestore();
+});
+
 const GoodBoy = () => {
     return <div>{goodBoyText}</div>;
 };
@@ -21,6 +32,11 @@ describe('<ErrorBoundary />', () => {
             </ErrorBoundary>
         );
         expect(getByText(goodBoyText)).toBeInTheDocument();
+
+        // By mocking out console.error we may inadvertently miss out on
+        // logs due to real errors. Let's reduce that likelihood by adding
+        // an assertion for how frequently console.error should be called.
+        expect(console.error).toHaveBeenCalledTimes(0);
     });
 
     it('renders the fallback UI when the child throws an error', () => {
@@ -30,5 +46,6 @@ describe('<ErrorBoundary />', () => {
             </ErrorBoundary>
         );
         expect(getByText(badBoyText)).toBeInTheDocument();
+        expect(console.error).toHaveBeenCalledTimes(2);
     });
 });
