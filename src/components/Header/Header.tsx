de7b7bd0024a4fc79e81ff1a6e32588ed014cc2f
Naresh Bhatia <naresh.archfirst@gmail.com>
2019-05-27 17:23:13 -0400

    Updated dependencies

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,30 +1,12 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import { makeStyles } from '@material-ui/styles';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import { Header as BaseHeader, HeaderTitle } from '@nareshbhatia/react-force';
 import { CompanySelect } from '..';
 
-const useStyles = makeStyles({
-    title: {
-        flex: 1
-    }
-});
-
 export const Header = () => {
-    const classes = useStyles();
     return (
-        <AppBar position="static">
-            <Toolbar>
-                <Typography
-                    variant="h6"
-                    color="inherit"
-                    className={classes.title}
-                >
-                    Stock Charts
-                </Typography>
-                {<CompanySelect />}
-            </Toolbar>
-        </AppBar>
+        <BaseHeader>
+            <HeaderTitle>Stock Charts</HeaderTitle>
+            <CompanySelect />
+        </BaseHeader>
     );
 };
